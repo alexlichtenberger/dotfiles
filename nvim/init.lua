67f874a39970e7fc197ccfadb6eb678f77bf7e89
alexlichtenberger <alex@alexlichtenberger.com>
2024-07-22 11:58:45 -0400

    added noice to nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -188,43 +188,43 @@ require("lazy").setup({
       -- Telescope picker. This is really useful to discover what Telescope can
       -- do as well as how to actually do it!
 
-			-- [[ Configure Telescope ]]
-			require("telescope").setup({
-				defaults = {
-					file_ignore_patters = {
-						".git",
-						"node_modules",
-						"build",
-						"dist",
-					},
-				},
-				pickers = {
-					find_files = {
-						hidden = true,
-						find_command = {
-							"rg",
-							"--files",
-							"--hidden",
-							"--glob=!**/.git/*",
-							"--glob=!**/.idea/*",
-							"--glob=!**/.vscode/*",
-							"--glob=!**/node_modules/*",
-							"--glob=!**/build/*",
-							"--glob=!**/dist/*",
-							"--glob=!**/yarn.lock",
-							"--glob=!**/package-lock.json",
-						},
-					},
-				},
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown(),
-					},
+      -- [[ Configure Telescope ]]
+      require("telescope").setup({
+        defaults = {
+          file_ignore_patters = {
+            ".git",
+            "node_modules",
+            "build",
+            "dist",
+          },
+        },
+        pickers = {
+          find_files = {
+            hidden = true,
+            find_command = {
+              "rg",
+              "--files",
+              "--hidden",
+              "--glob=!**/.git/*",
+              "--glob=!**/.idea/*",
+              "--glob=!**/.vscode/*",
+              "--glob=!**/node_modules/*",
+              "--glob=!**/build/*",
+              "--glob=!**/dist/*",
+              "--glob=!**/yarn.lock",
+              "--glob=!**/package-lock.json",
+            },
+          },
+        },
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown(),
+          },
           file_browser = {
             hijack_netrw = true
           }
-				},
-			})
+        },
+      })
 
       -- Enable Telescope extensions if they are installed
       pcall(require("telescope").load_extension, "fzf")
@@ -538,6 +538,42 @@ require("lazy").setup({
     -- HACK:
     -- FIX:
     -- PERF:
+  },
+
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    },
+    config = function()
+      require("noice").setup({
+        lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+          },
+        },
+        -- you can enable a preset for easier configuration
+        presets = {
+          bottom_search = true,         -- use a classic bottom cmdline for search
+          command_palette = true,       -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = false,       -- add a border to hover docs and signature help
+        },
+      })
+    end,
   },
 
   { -- Collection of various small independent plugins/modules
