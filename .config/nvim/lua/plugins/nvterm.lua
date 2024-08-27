ebaa2a2ca4351aae9d81c19cecb7f79d3a2960e2
alexlichtenberger <alex@alexlichtenberger.com>
2024-08-27 18:06:49 -0400

    added simpler bindings to nvterm for floating term

--- .config/nvim/lua/plugins/nvterm.lua
+++ .config/nvim/lua/plugins/nvterm.lua
@@ -3,9 +3,13 @@ return {
   config = function()
     require("nvterm").setup()
 
+    vim.keymap.set("n", "<A-t><A-t>", function() require("nvterm.terminal").toggle('float') end,
+      { desc = "[T]erminal Toggle [F]loat" })
     vim.keymap.set("n", "<A-t>f", function() require("nvterm.terminal").toggle('float') end,
       { desc = "[T]erminal Toggle [F]loat" })
     vim.keymap.set("t", "<A-t>f", function() require("nvterm.terminal").toggle('float') end,
+      { desc = "[T]erminal Toggle [F]loat" })
+    vim.keymap.set("t", "<esc><esc>", function() require("nvterm.terminal").toggle('float') end,
       { desc = "[T]erminal Toggle [F]loat" })
     vim.keymap.set("n", "<A-t>h", function() require("nvterm.terminal").toggle('horizontal') end,
       { desc = "[T]erminal Toggle [H]orizontal" })
@@ -17,11 +21,15 @@ return {
       { desc = "[T]erminal Toggle [V]ertical" })
     vim.keymap.set("n", "<A-t>nf", function() require("nvterm.terminal").new('float') end,
       { desc = "[T]erminal [N]ew [F]loat" })
+    vim.keymap.set("n", "<A-t>nn", function() require("nvterm.terminal").new('float') end,
+      { desc = "[T]erminal [N]ew [F]loat" })
     vim.keymap.set("n", "<A-t>nh", function() require("nvterm.terminal").new('horizontal') end,
       { desc = "[T]erminal [N]ew [H]orizontal" })
     vim.keymap.set("n", "<A-t>nv", function() require("nvterm.terminal").new('vertical') end,
       { desc = "[T]erminal [N]ew [V]ertical" })
     vim.keymap.set("n", "<A-t>ef", function() require("nvterm.terminal").send(vim.fn.input('Execute:'), 'float') end,
+      { desc = "[T]erminal [E]xecute [F]loat" })
+    vim.keymap.set("n", "<A-t>ee", function() require("nvterm.terminal").send(vim.fn.input('Execute:'), 'float') end,
       { desc = "[T]erminal [E]xecute [F]loat" })
     vim.keymap.set("n", "<A-t>eh", function() require("nvterm.terminal").send(vim.fn.input('Execute:'), 'horizontal') end,
       { desc = "[T]erminal [E]xecute [H]orizontal" })
